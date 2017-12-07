23bb31897bd6e56d91c58bda329f4ebd8ad4f470
TUDOR\Tudor Patrutiu <patrutiu.tudor@gmail.com>
2017-12-07 19:11:32 +0200

    first test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,2 +1,12 @@
 public class Test {
+
+    @Test
+    public void testPushAndPop() {
+        LinkListStack<Integer> st = new LinkListStack<>();
+        st.push(52);
+        st.push(40);
+        st.push(30);
+        assertEquals("190 70 50", st.toString());
+        assertEquals(190, (int) st.pop());
+        assertEquals("70 50", st.toString());
 }
